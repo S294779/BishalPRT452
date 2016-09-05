fa7cb818d08ba59f13e4fbbb70cf0ee69e018a4e
bbstha <bbstha143@gmail.com>
2016-09-05 20:36:54 +0930

    Anagram
    Anagrams Project

---
+++ AnagramsPT452/AnagramsPT452/AnagramChecker.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramsPT452
+{
+    public class AnagramChecker
+    {
+        public static string[] words;
+        static void Main(string[] args)
+        {
+            int totalno = 0;
+            int z = 1;
+            //Input number of words to check anagrams
+            
+            try {
+                Console.WriteLine("Input number of words to check anagrams");
+                totalno = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Input should be integer");
+
+            }
+                string[] words = new string[totalno];
+            for (int i = 0; i < totalno; i++)
+            {
+                //Take words from user to check anagrams
+                Console.WriteLine("Enter word" + " " + z++);
+                words[i] = Console.ReadLine();
+            }
+            List<string> result = new List<string>();
+            bool match = false;
+
+            //trim all spaces from the input words
+            for (int i = 0; i < words.Count(); i++)
+                words[i] = words[i].Trim();
+
+            //loop through array starting with first word
+            for (int i = 0; i < words.Count(); i++)
+            {
+                result.Add(words[i]);
+                //loop through array starting with the next word in line
+                for (int c = i + 1; c < words.Count(); c++)
+                {
+                    //only see if words match, if number of letters matches
+                    if (words[i].Length == words[c].Length && words[i] != "")
+                    {
+                        //convert both words to charArray
+                        char[] a = words[i].ToUpper().ToCharArray();
+                        char[] b = words[c].ToUpper().ToCharArray();
+                        Array.Sort(a);
+                        Array.Sort(b);
+
+                        match = false;
+                        int counter = 0;
+
+                        //loop throu each letter in both words
+                        foreach (char x in a)
+                        {
+                            if (x == b[counter])
+                                match = true;
+                            else
+                            {
+                                match = false;
+                                break;
+                            }
+                            counter++;
+                        }
+                        //if all letters match, add the words to the list
+                        if (match)
+                        {
+                            result.Add(words[c]);
+                            words[c] = "";
+                        }
+                    }
+                }
+
+                //if list is equal to 1, no match was found
+                if (result.Count() > 0 && result[0] != "")
+                {
+                    Console.Write("Anagrams: ");
+                    foreach (string s in result)
+                        Console.Write(s + " ");
+                    Console.WriteLine();
+                    Console.WriteLine(result.Count);
+                }
+                //reset the list
+                result.Clear();
+            }
+            Console.ReadKey();
+        }
+        public static void CheckAnagramWords(string[] paramWords)
+        {
+            string[] words = paramWords;
+
+            List<string> result = new List<string>();
+            bool match = false;
+
+            //trim all spaces from the input words
+            for (int i = 0; i < words.Count(); i++)
+                words[i] = words[i].Trim();
+
+            //loop through array starting with first word
+            for (int i = 0; i < words.Count(); i++)
+            {
+                result.Add(words[i]);
+                //loop through array starting with the next word in line
+                for (int c = i + 1; c < words.Count(); c++)
+                {
+                    //only see if words match, if number of letters matches
+                    if (words[i].Length == words[c].Length && words[i] != "")
+                    {
+                        //convert both words to charArray
+                        char[] a = words[i].ToUpper().ToCharArray();
+                        char[] b = words[c].ToUpper().ToCharArray();
+                        Array.Sort(a);
+                        Array.Sort(b);
+
+                        match = false;
+                        int counter = 0;
+
+                        //loop throu each letter in both words
+                        foreach (char x in a)
+                        {
+                            if (x == b[counter])
+                                match = true;
+                            else
+                            {
+                                match = false;
+                                break;
+                            }
+                            counter++;
+                        }
+                        //if all letters match, add the words to the list
+                        if (match)
+                        {
+                            result.Add(words[c]);
+                            words[c] = "";
+                        }
+                    }
+                }
+
+                //if list is equal to 1, no match was found
+                if (result.Count() > 0 && result[0] != "")
+                {
+                    Console.Write("Anagrams: ");
+                    foreach (string s in result)
+                        Console.Write(s + " ");
+                    Console.WriteLine();
+                    Console.WriteLine(result.Count);
+                }
+                //reset the list
+                result.Clear();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AnagramsPT452/UnitTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnagramsPT452;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] a = { "how ", "who", "awp", "paw", "wap  ", "12as","as12" };
+            AnagramChecker.CheckAnagramWords(a);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string[] b = { "cat ", "tac", "ac t", "i  nk", "nki  ", "pot" };
+            AnagramChecker.CheckAnagramWords(b);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string[] c = { "egg ", "geg", "ant", "tna", "atn  ", "hot","gge" };
+            AnagramChecker.CheckAnagramWords(c);
+        }
+    }
+}
